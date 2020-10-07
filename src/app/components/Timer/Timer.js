a0dabf7f3860f4d7227fde290018ac25ce9a5d73
crystal <969274162@qq.com>
2020-10-07 14:36:18 +0800

    [fangni] feat: implement timer html

--- src/app/components/Timer/Timer.js
+++ src/app/components/Timer/Timer.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./timer.less";
 
 class Timer extends Component {
   render() {
     return (
       <div className="timer">
-        This is a timer page
-        <p>
-          <Link to="/">
-            回到主页
-          </Link>
-        </p>
+        <section className="timer-content">
+          <h1>在线倒计时器</h1>
+          <section className="timer-board">
+            <section className="setting">
+              <div>
+                <label>设置时间</label>
+                <input placeholder="请输入设置的倒计时时间！"></input>
+              </div>
+              <button>Start</button>
+            </section>
+            <section className="count-down">
+              <p>Seconds</p>
+            </section>
+          </section>
+          <p>
+            <Link className="back-home" to="/">
+              回到主页
+            </Link>
+          </p>
+        </section>
       </div>
     );
   }
